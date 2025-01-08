1bdd404bcf50e9efe08c95f17270478eee22acfe
Moses <150498910+mosowaz@users.noreply.github.com>
2025-01-07 20:22:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euxo pipefail          # fail hard incase of any errors or typo
-
+######## Checkov Installation ########
 sudo apt update
 sudo apt install pipx       
 pipx install checkov        # by default packages installed with pipx are located in "$USER/.local/bin"
